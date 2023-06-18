b597a13cde080c8452273004112bd894780937c0
dudyaosuplayer <sasch2010pavlov@mail.ru>
2023-06-18 13:40:14 +0400

    commit3

--- NewsPaper/publication/views.py
+++ NewsPaper/publication/views.py
@@ -1,11 +1,18 @@
 from django.views.generic import ListView, DetailView
 from .models import Author, Category, Post, PostCategory, Comment
+from datetime import datetime
 
 
 class PostsList(ListView):
     model = Post
     template_name = 'news.html'
     context_object_name = 'posts'
+    queryset = Post.objects.order_by('-time_create')
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['time_now'] = datetime.utcnow()
+    #     return context
 
 
 class PostDetail(DetailView):
