1874374ef8aa26bd98363cefd904a0faf7e6b722
dudyaosuplayer <sasch2010pavlov@mail.ru>
2023-06-20 13:30:14 +0400

    commit6

--- NewsPaper/publication/templatetags/custom_filters.py
+++ NewsPaper/publication/templatetags/custom_filters.py
@@ -1,4 +1,5 @@
 import os
+import re
 from django import template
 from django.conf import settings
 
@@ -7,15 +8,15 @@ register = template.Library()
 
 @register.filter(name='censor')
 def censor(value):
-    # file_path = os.path.join(settings.STATICFILES_DIRS[0], 'ban_words.txt')
-    # with open(file_path, 'r') as file:
-    #     words = [word.rstrip('\n') for word in file.readlines()]
-    words = ['жопа', 'из-за', ]
+    file_path = os.path.join(settings.STATICFILES_DIRS[0], 'ban_words.txt')
+    with open(file_path, 'r', encoding='utf-8') as file:
+        words = [word.rstrip('\n').lower() for word in file.readlines()]
 
     censored_string = value
 
     for word in words:
-        censored_string = censored_string.replace(word, '***')
+        pattern = r'\b' + re.escape(word) + r'\b'
+        censored_string = re.sub(pattern, '***', censored_string, flags=re.IGNORECASE)
 
     return censored_string
 
