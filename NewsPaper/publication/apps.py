3ac18f3ac48c0fdce841d14b5e92247a12d6b47a
Sasha Pavlov <sasch2010pavlov@mail.ru>
2023-07-15 15:23:44 +0400

    Added email notification for updated list of news in subscribed category bu signals.

--- NewsPaper/publication/apps.py
+++ NewsPaper/publication/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class NewsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'publication'
+
+    def ready(self):
+        from publication import signals
---
+++ NewsPaper/publication/signals.py
@@ -0,0 +1,38 @@
+from django.core.mail import EmailMultiAlternatives
+from django.template.loader import render_to_string
+from django.db.models.signals import post_save, m2m_changed
+from django.dispatch import receiver
+from .models import PostCategory, Post
+from static.config import DEFAULT_FROM_EMAIL
+
+
+@receiver(m2m_changed, sender=Post.category.through)
+def new_post_notify_user(sender, instance, action, **kwargs):
+    # Новая статья была создана
+    if action == 'post_add':
+        subscribers = instance.category.values(
+            'subscribers__email', 'subscribers__username'
+        )
+        for subscriber in subscribers:
+            # email = subscriber['subscribers__email']
+            # Генерация HTML-сообщения из шаблона
+            html = render_to_string(
+                'mailing/new_post_notification.html',
+                {
+                    # 'category': category,
+                    'post': instance,
+                },
+            )
+
+            msg = EmailMultiAlternatives(
+                subject=f'Здравствуй, {subscriber.get("subscribers__username")}. Новая статья в твоём любимом разделе!',
+                body='',
+                from_email=DEFAULT_FROM_EMAIL,
+                to=[subscriber.get("subscribers__email")],
+            )
+
+            msg.attach_alternative(html, 'text/html')
+            try:
+                msg.send()
+            except Exception as e:
+                print(e)
